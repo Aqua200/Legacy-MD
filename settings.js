45c4e45b055a3f69ed36cc87a3f04c356f81fcef
Neykoor💜 <chinquepapa@gmail.com>
2025-10-08 04:05:55 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.prems = []
 
 global.libreria = "Baileys Multi Device"
 global.vs = "^1.8.2|Latest"
-global.nameqr = "YukiBot-MD"
+global.nameqr = "Ouka-MD"
 global.sessions = "Sessions/Principal"
 global.jadi = "Sessions/SubBot"
 global.OukaJadibts = true
